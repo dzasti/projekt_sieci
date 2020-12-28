ab4779d8ef4c4682d2899a5f7015c035e43e5b99
Justyna Wojsz <just.justa7@gmail.com>
2020-12-28 12:46:04 +0100

    wszystko ok do loginu

--- serwer.py
+++ serwer.py
@@ -3,14 +3,14 @@ import os
 from _thread import *
 import re
 import random
+import threading 
+import numpy as np
 
 ServerSideSocket = socket.socket()
 host = '127.0.0.1'
 port = 2004
 ThreadCount = 0
 listOfIndexes = []
-#listOfClients = []
-order = []
 threads = []
 
 try:
@@ -20,45 +20,59 @@ except socket.error as e:
 
 ServerSideSocket.listen(4)
 
-def multi_threaded_client(connection):
-    connection.send(str.encode('CONNECTED'))
-    while True:
-        data = connection.recv(2048)
-        response = data.decode('utf-8')
-        regex = re.compile('LOGIN ......')
-        match = 'no'
-        for string in listOfIndexes:
-            if response == string:
-                match = 'yes'
-        if re.match(regex, response) and match == 'no':
-            connection.send(str.encode('OK'))
-            global ThreadCount
-            ThreadCount = ThreadCount + 1
-            listOfIndexes.append([response,ThreadCount,connection])
-            #print('Thread Number: ' + str(ThreadCount))
-            break
-        else:
-            connection.send(str.encode('ERROR')) 
+class separete_client(threading.Thread):
+
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+
+    def run(self):
+        connection.send(str.encode('CONNECTED'))
+        while True:
+            data = connection.recv(2048)
+            response = data.decode('utf-8')
+            regex = re.compile('LOGIN ......')
+            match = 'no'
+            for string in listOfIndexes:
+                if response == string:
+                    match = 'yes'
+            if re.match(regex, response) and match == 'no':
+                connection.send(str.encode('OK'))
+                global ThreadCount
+                ThreadCount = ThreadCount + 1
+                listOfIndexes.append([response,ThreadCount,connection])
+                break
+            else:
+                connection.send(str.encode('ERROR')) 
 
 for x in range(0,4):
-    Client, address = ServerSideSocket.accept()
-    t = start_new_thread(multi_threaded_client, (Client, ))
-	thread.append(t)
-    #listOfClients.append(Client)
+    (connection, (ip,port)) = ServerSideSocket.accept()
+    newthread = separete_client(ip,port)
+    newthread.start() 
+    threads.append(newthread)
 
-for x in threads:
-	x.join()
+for t in threads: 
+    t.join() 
 
-#random.shuffle(ListOfIndexes[])
+##########################################   end  of login #########################################################
 
+
+order = []
 order = np.random.permutation([1,2,3,4])
 y = 0
 
-for x in ListOfIndexes:
+print(order)
+
+for x in listOfIndexes:
     x[1] = order[y]
     y = y + 1
 
-for x in ListOfIndexes:
+print(listOfIndexes)
+
+"""
+for x in listOfIndexes:
     x[2].send(str.encode('START ' + x[1] + " " + order ))
+"""
 
 ServerSideSocket.close()
